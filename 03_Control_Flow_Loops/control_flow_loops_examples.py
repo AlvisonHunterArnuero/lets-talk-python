797881c2dafd7308bd84ec14b0eb2ab748f6c2bc
AlvisonHunterArnuero <alvison@gmail.com>
2023-05-27 12:23:09 -0600

    switch case simulator using dicts

--- 03_Control_Flow_Loops/control_flow_loops_examples.py
+++ 03_Control_Flow_Loops/control_flow_loops_examples.py
@@ -9,3 +9,20 @@ else:
 # Ternary Operator Example
 gender = "B"
 print("Masculine" if gender=="M" else "Femenine")
+
+# Switch case Example (Simulation since this only exist on ver 3.10)
+
+term_to_pass = "python"
+def switch_case(search_term: str) -> str:
+    search_term = search_term.upper()
+    if (search_term == "JAVASCRIPT"):
+        return ("You can be a web developer")
+    elif (search_term == "JAVA"):
+        return ("You can be a mobile app developer")
+    elif (search_term == "SVELTE"):
+        return ("You can be a great web developer")
+    elif (search_term == "PYTHON"):
+        return ("You can be a Data Scientist")
+    else:
+        return ("You can be what you want!")
+print(switch_case(term_to_pass))
