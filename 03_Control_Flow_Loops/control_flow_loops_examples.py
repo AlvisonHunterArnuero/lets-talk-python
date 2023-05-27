b1a4f7d0049547bda30467904741b314b7b55053
AlvisonHunterArnuero <alvison@gmail.com>
2023-05-27 12:25:51 -0600

    Second Switch Example using dicts

--- 03_Control_Flow_Loops/control_flow_loops_examples.py
+++ 03_Control_Flow_Loops/control_flow_loops_examples.py
@@ -26,3 +26,18 @@ def switch_case(search_term: str) -> str:
     else:
         return ("You can be what you want!")
 print(switch_case(term_to_pass))
+
+# Second Example using Dictionaries
+spoken_lang_dict = {"English": "You are from England", "Spanish": "You are from Nicaragua",
+                    "French": "You are from France", "Portuguese": "You are from Brazil"}
+
+def nationality(spoken_language: str)-> str:
+    spoken_language = spoken_language.title()
+    if spoken_language in spoken_lang_dict:
+        return(spoken_lang_dict[spoken_language])
+    else:
+        return "We don't know where you come from!"
+
+spoken_language = "French"
+print(nationality(spoken_language))
+
