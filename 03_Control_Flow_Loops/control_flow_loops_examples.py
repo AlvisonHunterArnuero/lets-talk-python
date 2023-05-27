27ae479839c4dd55e9b21194346653c15b00e636
AlvisonHunterArnuero <alvison@gmail.com>
2023-05-27 12:33:11 -0600

    Formatting applied to code

--- 03_Control_Flow_Loops/control_flow_loops_examples.py
+++ 03_Control_Flow_Loops/control_flow_loops_examples.py
@@ -1,4 +1,4 @@
-# --------  Conditional if with several cases -------- 
+# --------  Conditional if with several cases --------
 num = 1
 if num > 3:
     print("Numero Mayor que 3")
@@ -7,13 +7,15 @@ elif num == 3:
 else:
     print("Numero menor que 3")
 
-# -------- Ternary Operator Example -------- 
+# -------- Ternary Operator Example --------
 gender = "B"
-print("Masculine" if gender=="M" else "Femenine")
+print("Masculine" if gender == "M" else "Femenine")
 
 # Switch case Example (Simulation since this only exist on ver 3.10)
 
 term_to_pass = "python"
+
+
 def switch_case(search_term: str) -> str:
     search_term = search_term.upper()
     if (search_term == "JAVASCRIPT"):
@@ -26,51 +28,55 @@ def switch_case(search_term: str) -> str:
         return ("You can be a Data Scientist")
     else:
         return ("You can be what you want!")
+
+
 print(switch_case(term_to_pass))
 
-# --------  Second Example using Dictionaries -------- 
+# --------  Second Example using Dictionaries --------
 spoken_lang_dict = {"English": "You are from England", "Spanish": "You are from Nicaragua",
                     "French": "You are from France", "Portuguese": "You are from Brazil"}
 
-def nationality(spoken_language: str)-> str:
+
+def nationality(spoken_language: str) -> str:
     spoken_language = spoken_language.title()
     if spoken_language in spoken_lang_dict:
-        return(spoken_lang_dict[spoken_language])
+        return (spoken_lang_dict[spoken_language])
     else:
         return "We don't know where you come from!"
+
 
 spoken_language = "French"
 print(nationality(spoken_language))
 
-# -------- Example using While Loop -------- 
+# -------- Example using While Loop --------
 count = 0
 while count < 5:
     print("Count is: ", count)
     count += 1
 
-# -------- For Loops with Range -------- 
-for num in range(1,10,3):
+# -------- For Loops with Range --------
+for num in range(1, 10, 3):
     print("Number is: ", num)
 
-favorite_fruits = ["apple","banana","cherry","mango","pear","watermelon","pineapple"]
+favorite_fruits = ["apple", "banana", "cherry", "mango", "pear", "watermelon", "pineapple"]
 for fruit in favorite_fruits:
     print(fruit)
 
-for fruit_index in range(1,len(favorite_fruits),2):
+for fruit_index in range(1, len(favorite_fruits), 2):
     print(favorite_fruits[fruit_index])
 
-# -------- For loop using strings -------- 
+# -------- For loop using strings --------
 message = "Nicaragua"
 for char in message:
     print("Character: ", char)
 
-# -------- Nested for loops -------- 
-for i in range(1,4):
-    for j in range(1,4):
+# -------- Nested for loops --------
+for i in range(1, 4):
+    for j in range(1, 4):
         print("is is: ", i, "j is: ", j)
 
-# -------- For loops with break -------- 
-numbers = [1,2,3,4,5,6]
+# -------- For loops with break --------
+numbers = [1, 2, 3, 4, 5, 6]
 for num in numbers:
     if num == 5:
         print("Finished loop on number ", num)
@@ -83,13 +89,13 @@ for num2 in numbers:
         continue
     print("Current number is: ", num2)
 
-# -------- For loop with else -------- 
+# -------- For loop with else --------
 for n in numbers:
     print("Current number is : ", n)
 else:
     print("For loop completed")
 
-# -------- For loop with enumerate -------- 
-countries = ["Nicaragua","Argentina", "Colombia", "Costa Rica"]
+# -------- For loop with enumerate --------
+countries = ["Nicaragua", "Argentina", "Colombia", "Costa Rica"]
 for index, country in enumerate(countries):
     print("Index: ", index, "Country: ", country)
